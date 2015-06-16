da43a51155515a731878461ead1ba58b50f1a441
Masamoto Miyata <miyata@sincere-co.com>
2015-06-16 14:44:46 +0900

    inial commit

---
+++ NUnitTest/SampleTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace NUnitTest
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.AreEqual("aaa", "aaa");
+        }
+
+        [Test]
+        public void FailTestMethod()
+        {
+            Assert.AreEqual("ccc", "aaa");
+        }
+    }
+}
